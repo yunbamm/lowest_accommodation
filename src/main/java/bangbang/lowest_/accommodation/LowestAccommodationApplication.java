770fa24ef364a730a968966cd98ecd4cf4896fc3
bangbang <fdsajklt2@naver.com>
2025-09-04 23:34:07 +0900

    Initial commit

---
+++ src/main/java/bangbang/lowest_/accommodation/LowestAccommodationApplication.java
@@ -0,0 +1,13 @@
+package bangbang.lowest_.accommodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowestAccommodationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowestAccommodationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bangbang/lowest_/accommodation/LowestAccommodationApplicationTests.java
@@ -0,0 +1,13 @@
+package bangbang.lowest_.accommodation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowestAccommodationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
